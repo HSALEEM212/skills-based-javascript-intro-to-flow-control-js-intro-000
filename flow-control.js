975e1d5ae98e78c929e3db0149c68ff902fd54b5
Hassan Saleem <hassansaleem2@outlook.com>
2019-09-25 21:29:15 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,26 +28,8 @@ return (age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-  switch(age) {
-    case 13:
-    console.log("You are a teenager")
-    break;
-    case 14:
-    console.log("You are a teenager")
-    break;
-    case 15:
-    console.log("You are a teenager")
-    break;
-    case 16:
-    console.log("You are a teenager")
-    break;
-    case 17:
-    console.log("You are a teenager")
-    break;
-    case 18:
-    console.log("You are a teenager")
-    break;
-    case 19:
+  switch(switchAge) {
+    case age >=13 && age <= 19:
     console.log("You are a teenager")
     break;
     default:
