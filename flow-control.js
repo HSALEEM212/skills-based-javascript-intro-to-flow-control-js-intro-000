98ea6c476b1bb0287fdc7decf1301907c7ddd79c
Hassan Saleem <hassansaleem2@outlook.com>
2019-09-25 21:18:49 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,5 +28,12 @@ return (age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
+  switch(age) {
+    case 13:
+    console.log("You are a teenager")
+    break;
+    default:
+    console.log("You have an age")
+  }
 
 }
